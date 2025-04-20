7f20aff1504aca0d762a09029216f56d01591be9
arnaud-ma <arnaudma.code@gmail.com>
2025-04-20 06:04:41 +0200

    refactorise code: utilise l'inheritance de django au lieu des relations Onetone

--- agence/models.py
+++ agence/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.forms import CharField
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+# ---------------------------------------------------------------------------- #
+#                              region localisation                             #
+# ---------------------------------------------------------------------------- #
 
 
 class Lieu(models.Model):
@@ -15,6 +21,20 @@ class Lieu(models.Model):
         return f"{self.adresse} ({self.code_commune}): {(self.latitude, self.longitude)})"
 
 
+class Agence(models.Model):
+    nom = CharField(max_length=255)
+    id_lieu = models.ForeignKey(Lieu, models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nom} située à {self.id_lieu}"
+
+
+# endregion
+# ---------------------------------------------------------------------------- #
+#                                  region Bien                                 #
+# ---------------------------------------------------------------------------- #
+
+
 class InfosBien(models.Model):
     nb_chambres = models.IntegerField(null=True)
     nb_salles_bain = models.IntegerField(null=True)
@@ -23,15 +43,42 @@ class InfosBien(models.Model):
     nb_wc = models.IntegerField(null=True)
     surface_habitable = models.FloatField(null=True)
     surface_terrain = models.FloatField(null=True)
-    id_lieu = models.ForeignKey(Lieu, models.PROTECT, null=True)
+    lieu = models.ForeignKey(Lieu, models.PROTECT, null=True)
 
     def __str__(self):
         return (
-            f"InfosBien à {self.id_lieu}: "
+            f"InfosBien à {self.lieu}: "
             f"{self.nb_chambres} chambres, "
             f"{self.surface_habitable} m² habitable, "
             f"{self.surface_terrain} m² terrain"
         )
+
+
+class Bien(models.Model):
+    class Etat(models.TextChoices):
+        PROSPECTION = "PR"
+        ESTIMATION = "ES"
+        MISE_EN_VENTE = "MV"
+        SIGNATURE_COMPROMIS = "SC"
+        SIGNATURE_VENTE = "SV"
+
+    etat = models.CharField(
+        max_length=2, choices=Etat.choices, default=Etat.PROSPECTION
+    )
+    infos_bien = models.ForeignKey(InfosBien, models.PROTECT, null=True)
+    vendeur = models.ForeignKey("Vendeur", models.CASCADE)
+
+    def __str__(self):
+        attrs = {"vendeur": self.vendeur, "etat_bien": self.etat.label}
+        if self.infos_bien:
+            attrs["infos_bien"] = self.infos_bien
+        return ", ".join(f"{k}: {v}" for k, v in attrs.items()) + f" ({self.pk})"
+
+
+# endregion
+# ---------------------------------------------------------------------------- #
+#                              region Utilisateur                              #
+# ---------------------------------------------------------------------------- #
 
 
 class Utilisateur(models.Model):
@@ -41,46 +88,36 @@ class Utilisateur(models.Model):
     email = models.EmailField()
 
     def __str__(self):
-        return f"{self.prenom} {self.nom} ({self.email})"
+        coords = (self.email, self.telephone)
+        coords = filter(None, coords)
+        coords_str = ", ".join(map(str, coords))
+        return f"{self.prenom} {self.nom} ({coords_str})"
 
 
-class Acheteur(models.Model):
-    id_utilisateur = models.OneToOneField(Utilisateur, models.CASCADE, primary_key=True)
-    id_critere_recherche = models.ForeignKey(InfosBien, models.CASCADE, null=True)
-
-    def __str__(self):
-        return f"Compte acheteur de {self.id_utilisateur}"
+class Vendeur(Utilisateur):
+    pass
 
 
-class Vendeur(models.Model):
-    id_utilisateur = models.OneToOneField(Utilisateur, models.CASCADE, primary_key=True)
-
-    def __str__(self):
-        return f"Compte vendeur de {self.id_utilisateur}"
+class Acheteur(Utilisateur):
+    critere_recherche = models.ForeignKey(InfosBien, models.CASCADE, null=True)
 
 
-class Bien(models.Model):
-    class EtatBien(models.TextChoices):
-        PROSPECTION = "PR"
-        ESTIMATION = "ES"
-        MISE_EN_VENTE = "MV"
-        SIGNATURE_COMPROMIS = "SC"
-        SIGNATURE_VENTE = "SV"
-
-    etat_bien = models.CharField(
-        max_length=2, choices=EtatBien.choices, default=EtatBien.PROSPECTION
-    )
-    infos_bien = models.ForeignKey(InfosBien, models.PROTECT, null=True)
-    id_vendeur = models.ForeignKey(Vendeur, models.CASCADE)
+class Agent(Utilisateur):
+    agence = models.ForeignKey(Agence, models.CASCADE)
 
     def __str__(self):
-        result = "" if self.infos_bien is None else f"Bien à {self.infos_bien.id_lieu}: "
-        return f"{result}etat:{self.etat_bien.label}, vendeur: {self.id_vendeur}"
+        return f"{super().__str__()}, agence: {self.agence}"
+
+
+# endregion
+# ---------------------------------------------------------------------------- #
+#                      region Interaction acheteur - bien                      #
+# ---------------------------------------------------------------------------- #
 
 
 class FaitAchat(models.Model):
-    id_bien = models.ForeignKey(Bien, models.CASCADE)
-    id_acheteur = models.ForeignKey(Acheteur, models.CASCADE)
+    bien = models.ForeignKey(Bien, models.CASCADE)
+    acheteur = models.ForeignKey(Acheteur, models.CASCADE)
 
     class EtapeAchat(models.IntegerChoices):
         PROSPECTION = 1
@@ -95,11 +132,14 @@ class FaitAchat(models.Model):
         REFUSE = 10, "Refusé"
         ABANDON = 11
 
-    etape_achat = models.IntegerField(choices=EtapeAchat)
+    etape_achat = models.IntegerField(choices=EtapeAchat.choices)
 
     def __str__(self):
         return (
-            f"Fait achat de {self.id_acheteur.id_utilisateur} "
-            f"pour le bien {self.id_bien} "
-            f"({self.etape_achat})"
+            f"Fait achat de {self.acheteur} "
+            f"pour le bien {self.bien} "
+            f"({self.etape_achat.label})"
         )
+
+
+# endregion
