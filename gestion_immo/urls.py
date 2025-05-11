583481fac6ff9c4cff2025acd85ded2ea2557f30
arnaud-ma <arnaudma.code@gmail.com>
2025-05-11 20:57:56 +0200

    url racine renvoie à l'url agence/

--- gestion_immo/urls.py
+++ gestion_immo/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", include("agence.urls")),
+    path("", RedirectView.as_view(url="agence/")),
     path("agence/", include("agence.urls")),
     path("admin/", admin.site.urls),
 ]
