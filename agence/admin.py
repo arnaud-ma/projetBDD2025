0db906d0290186a1842f4b16a6b87203b3225ece
arnaud-ma <arnaudma.code@gmail.com>
2025-05-11 21:55:16 +0200

    enregistre les mddèles dans le panel admin

--- agence/admin.py
+++ agence/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from .models import (
+    Acheteur,
+    Agence,
+    Agent,
+    Avis,
+    Bien,
+    FaitAchat,
+    InfosBien,
+    Lieu,
+    Message,
+    RendezVous,
+    Utilisateur,
+    Vendeur,
+)
+
 # Register your models here.
+admin.site.register([
+    Utilisateur,
+    Acheteur,
+    Vendeur,
+    Agent,
+    Lieu,
+    Agence,
+    InfosBien,
+    Bien,
+    FaitAchat,
+    RendezVous,
+    Avis,
+    Message,
+])
